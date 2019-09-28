6c0c72ae3e5a6c5eb5c2b806b4f51340f5676a83
Guilherme Demoliner <guillherme.demoliner@gmail.com>
2019-09-27 23:13:04 -0300

    Adicionadas novas seções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,25 @@ import {DomSanitizer} from "@angular/platform-browser";
 import {MatIconRegistry} from "@angular/material";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'feiraDasProfissoes';
+    title = 'feiraDasProfissoes';
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-      'map-pointer',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/map_pointer.svg'));
-  }
+    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+        iconRegistry.addSvgIcon(
+            'map-pointer',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/map_pointer.svg'));
+        iconRegistry.addSvgIcon(
+            'student-hat',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/student_hat.svg'));
+        iconRegistry.addSvgIcon(
+            'map',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/map_simple.svg'));
+        iconRegistry.addSvgIcon(
+            'clock',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/clock.svg'));
+    }
 }
--- src/pages/principal/principal.component.ts
+++ src/pages/principal/principal.component.ts
@@ -2,34 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import {SwiperOptions} from "swiper";
 
 @Component({
-  selector: 'app-principal',
-  templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.scss']
+    selector: 'app-principal',
+    templateUrl: './principal.component.html',
+    styleUrls: ['./principal.component.scss']
 })
 export class PrincipalComponent implements OnInit {
+    public informacoesFeira = '';
+    public programacao = ''
+    public config : SwiperOptions;
+    public images = [];
+    constructor() {
 
-  public config : SwiperOptions;
-  public images = [];
-  constructor() { }
 
-  ngOnInit() {
-    this.images = [
-      {src:'assets/images/banner1.jpg'},
-      {src:'assets/images/banner2_1.jpg'},
-      {src:'assets/images/banner3_2.jpg'},
+        this.informacoesFeira='  O evento caracteriza‐se na realização de visitas para alunos concluintes do ensino médio, de escolas interessadas em conhecer a instituição, seu funcionamento, sua infraestrutura e as possibilidades de ingresso no ensino superior.\n' +
+            '\nEsta é uma excelente oportunidade para os estudantes de nossa região desenvolverem novas referências sobre um ensino superior de qualidade. É uma maneira agradável de entrar em contato com o mundo acadêmico, que está tão próximo desses alunos e, sobretudo, trata‐se de uma oportunidade ímpar de auxiliá‐los na tarefa de decidir pelo desenvolvimento de habilidades adequadas para a construção de uma sólida carreira profissional.\n' +
+            '\nPara participar, a escola deve agendar e confirmar período e horário definidos, por meio do e‐mail: feiraprofissoes.smo@unoesc.edu.br\n' +
+            '\nA partir da confirmação por parte da Escola, a organização da Feira das Profissões entrará novamente em contato para realizar a visita e para maiores informações.\n'
 
-    ]
 
-    this.config  = {
-      height: 300,
-      autoHeight:true,
-      pagination: {el: '.swiper-pagination', clickable: true},
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-      spaceBetween: 30
-    };
-  }
+        this.programacao =
+            `Saída da Escola à Unoesc São Miguel do Oeste, horário a ser definido com a escola.  <br>
+            Recepção: estacionamento 2 da Unoesc.<br>
+            Encaminhamentos para visitação aos estandes e laboratórios - Visitação livre pelo campus.<br>
+            Os alunos terão a oportunidade de conhecer cada local,conforme afinidade.<br>
+            • Palestra de encerramento: Escolha profissiona`
+    }
+
+
+    ngOnInit() {
+        this.images = [
+            {src: 'assets/images/banner1.jpg'},
+            {src: 'assets/images/banner2_1.jpg'},
+            {src: 'assets/images/banner3_2.jpg'},
+
+        ];
+
+        this.config  = {
+
+            autoHeight:false,
+            pagination: {el: '.swiper-pagination', clickable: true},
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+            spaceBetween: 30
+        };
+    }
 
 }
